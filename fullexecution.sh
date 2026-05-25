fb223e2c2fc9d4e2f5721fa2003c1c811f12b4c6
ruben <r@g.it>
2026-05-25 17:36:19 +0200

    module call in fullexec

--- fullexecution.sh
+++ fullexecution.sh
@@ -83,7 +83,7 @@ fi
 
 echo "Starting reconstruction..."
 
-python3 reco.py -i ${UNPACKED_FILE} \
+python3 -m ferrari_core.reco -i ${UNPACKED_FILE} \
     -r "$RUN" \
     -s "$SPILL" \
     -ro ${RECO_UNPACKED_OUTDIR}/reco/run_$RUN/ \
