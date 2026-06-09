a153829060d7c0ed99e127d253ef8136bf847835
ecalgit <ecalgit@cern.ch>
2026-06-09 14:39:12 +0200

    define envs dev

--- define_envs_dev.sh
+++ define_envs_dev.sh
@@ -5,9 +5,8 @@ export HOME_DIR="/afs/cern.ch/user/e/ecalgit/"
 export SPILL_HADD_INTERVAL=4
 export SPILL_LASER=3
 
-export PLOT_MAIN_FOLDER="/eos/user/m/mcampana/www/h4dqm/ECAL_TB_2026/"
+export PLOT_MAIN_FOLDER="/eos/user/m/mcampana/www/h4dqm/ECAL_TB_2026_dev/"
 export RECO_UNPACKED_OUTDIR="/eos/cms/store/group/dpg_ecal/comm_ecal/upgrade/testbeam/ECALTB_H4_Jun2026/"
-
 export UNPACKED_FOLDER="${RECO_UNPACKED_OUTDIR}/DataTree_dqm"
 
 export RAW_DIR="/eos/cms/store/group/dpg_ecal/comm_ecal/upgrade/testbeam/ECALTB_H4_Jun2026/EB/"
@@ -15,10 +14,33 @@ export LOGS_DIR="/eos/user/m/mcampana/www/h4dqm/ECAL_TB_2026/test_logs"
 
 export DANTE_DIR="/afs/cern.ch/user/e/ecalgit/EBeTe/"
 
-export WORKING_DIR="${HOME_DIR}/cms-ecal-h4-ferrari/"
-export JSON_CONF="${HOME_DIR}/cms-ecal-h4-ferrari/confs/detectors_conf.json"
-export PHP_FILES_DIR="${HOME_DIR}/cms-ecal-h4-ferrari/ferrari_core/php/"
-export PLOTS_PLUGINS_FOLDER="${HOME_DIR}/cms-ecal-h4-ferrari/custom_plot_functions/"
+export WORKING_DIR="${HOME_DIR}/cms-ecal-h4-ferrari-dev"
+export JSON_CONF="${HOME_DIR}/cms-ecal-h4-ferrari-dev/confs/detectors_conf_1bcp.json"
+export PHP_FILES_DIR="${HOME_DIR}/cms-ecal-h4-ferrari-dev/ferrari_core/php/"
+export PLOTS_PLUGINS_FOLDER="${HOME_DIR}/cms-ecal-h4-ferrari-dev/custom_plot_functions/"
 export NUMPY_UNPACKED_DIR="${HOME_DIR}/ecal-h4-utils"
 
-export UNPACKER_ROUTINE="DANTE"
+export UNPACKER_ROUTINE="NUMPY"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
