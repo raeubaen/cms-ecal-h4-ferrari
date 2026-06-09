a87c0b93d9920f74b2bc8f8b10396f8e25414468
ecalgit <ecalgit@cern.ch>
2026-06-09 14:23:54 +0200

    laser plots added, mem also, removes env sourcing in process_spill, commit ferrari_core

--- process_spill.sh
+++ process_spill.sh
@@ -1,14 +1,4 @@
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-echo "DEBUG: Running .sh inside $SCRIPT_DIR"
-
-cd ${SCRIPT_DIR}
-
-echo "INSIDE FOLDER: $(pwd)"
-
-#echo $(ls -alrt)
-
-source ${SCRIPT_DIR}/define_envs.sh
-
 cd ${WORKING_DIR}
+echo ${WORKING_DIR}
 
 timeout 30s ./fullexecution.sh "$@" 2>&1 | tee ${LOGS_DIR}/log_run${1}_spill${2}_typeis${3}.log
